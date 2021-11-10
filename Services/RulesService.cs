1906e5f4949b3e1573e1044874357e0e9b456e75
Alina Babenko <hacka@ukr.net>
2021-11-10 14:56:42 +0200

    Update Charge method

--- Services/RulesService.cs
+++ Services/RulesService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,11 @@ namespace RulesApi.Services
 {
     public class RulesService : IRules
     {
-        private const float _MAB = 5000; //minimum average balance (MAB)
+        private const float _MAB = 1000; //minimum average balance (MAB)
 
         public string evaluateMinBal(int accountId, float balance)
         {
+           
             switch (balance)
             {
                 case > _MAB:
@@ -28,15 +30,23 @@ namespace RulesApi.Services
 
         public float getServiceCharges(float balance)
         {
-            float withServiceCharge = balance;
+            try
+            {
+                float ServiceCharge = balance;
 
-            //Metro/Urban/Semi-Urban/Rural locations :
-            //Rs. 100 + 5 % of the shortfall in required MAB
+                //Metro/Urban/Semi-Urban/Rural locations :
+                //Rs. 100 + 5 % of the shortfall in required MAB
 
-            if (balance < _MAB)
-                withServiceCharge += 100 + ( _MAB - balance)* 0.05f;
-            
-            return withServiceCharge;
+                if (balance < _MAB)
+                    ServiceCharge = 100 + (_MAB - balance) * 0.05f;
+
+                return ServiceCharge;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            return 0;
         }
     }
 }
